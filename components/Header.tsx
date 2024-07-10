767db988e47d13774ed4b74ca8dc13a423ab17db
Harsh Dagar <amandagar000@gmail.com>
2024-07-11 04:31:32 +0530

    add current role

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,18 +10,19 @@ export default function Header({ visible = false }: HeaderProps) {
     <div
       data-visible={visible}
       className={
-        "group fixed py-5 px-7 top-0 w-full z-[1] opacity-0 transition-all backdrop-blur-sm shadow-2xl data-[visible=true]:opacity-100 data-[visible=false]:pointer-events-none bg-black bg-opacity-80"
+        "group fixed py-3 px-7 top-0 w-full z-[1] opacity-0 transition-all backdrop-blur-sm shadow-2xl data-[visible=true]:opacity-100 data-[visible=false]:pointer-events-none bg-black bg-opacity-80"
       }
     >
       <div className="flex justify-between relative">
         <h2 className="tracking-[11px] group-data-[visible=true]:animate-slideLeft text-nowrap">
           Harsh Dagar
+          <div className="tracking-wider text-sm">Sr. Software Engineer</div>
         </h2>
         <label htmlFor="menuTrigger" className="md:hidden">
           <Menu size={24} />
         </label>
         <input type="checkbox" id="menuTrigger" hidden />
-        <nav className="max-md:fixed max-md:isolation-auto max-md:-right-full max-md:transition-[right] max-md:bg-black max-md:bg-opacity-95 max-md:backdrop-blur-sm group-has-[:checked]:max-md:-right-0 max-md:pl-6 max-md:pr-10 max-md:text-2xl max-md:top-0 z-10 max-md:flex-col max-md:h-screen max-sm:w-full max-md:w-fit max-md:duration-300 flex gap-5 md:overflow-y-hidden *:translate-y-full">
+        <nav className="max-md:fixed max-md:isolation-auto max-md:-right-full max-md:transition-[right] max-md:bg-black max-md:bg-opacity-95 max-md:backdrop-blur-sm group-has-[:checked]:max-md:-right-0 max-md:pl-6 max-md:pr-10 max-md:text-2xl max-md:top-0 z-10 max-md:flex-col max-md:h-screen max-sm:w-full max-md:w-fit max-md:duration-300 flex gap-5 md:overflow-y-hidden *:translate-y-[150%] md:items-center">
           <label className="md:hidden mb-10" htmlFor="menuTrigger">
             <X size={32} className="ml-auto" />
           </label>
